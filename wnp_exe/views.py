675a7f7f225dab88caa7948c52f39c18fbade2d3
wesleynp1 <wesleynp987@gmail.com>
2026-06-03 19:53:01 -0300

    Pequena alteração na interface gráfica e botão de logout

--- wnp_exe/views.py
+++ wnp_exe/views.py
@@ -5,13 +5,15 @@ from django.contrib import auth
 
 def login(request):
     if request.method == "GET":
-        next = request.GET["next"] if request.GET else ""
-        return render(request,"login.html",{ "next" : next })
+        proxima_pagina = request.GET["next"] if "next" in request.GET else "index"
+        return render(request,"login.html",{ "next" : proxima_pagina })
     elif request.method == "POST":
         user = auth.authenticate(request ,username=request.POST["username"], password=request.POST["password"])
         if user is not None:
             auth.login(request, user)
-            return redirect("index")
+
+            proxima_pagina = request.POST["next"] if "next" in request.POST else "index"
+            return redirect(proxima_pagina)
         else:
             return render(request,"login.html",{"mensagem":"Autenticação falhou!"})
     else:
